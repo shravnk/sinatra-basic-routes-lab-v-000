efd2eb06eb5c2470c379b1954af56a1de0520eff
Brendan O'Dea <brendan.ode4@gmail.com>
2018-03-20 14:13:52 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -1,4 +1,16 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+  get '/name' do
+    'My name is Brendan.'
+  end
+
+  get '/hometown' do
+    'My hometown is New York'
+  end
+
+  get '/favorite-song' do
+    'My favorite song is A'
+  end
+
 end
